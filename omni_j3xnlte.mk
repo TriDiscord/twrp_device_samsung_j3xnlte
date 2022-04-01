7987651d742f752f5dc4bfb78c1d94bdffbc1da6
NotNoelChannel <52098241+NotNoelChannel@users.noreply.github.com>
2022-04-01 15:50:18 +0200

    Update omni_j3xnlte.mk

--- omni_j3xnlte.mk
+++ omni_j3xnlte.mk
@@ -18,13 +18,13 @@
 # $(call inherit-product, device/sample/products/backup_overlay.mk)
 
 # Get the prebuilt list of APNs
-$(call inherit-product, vendor/omni/config/gsm.mk)
+$(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/cm/config/common.mk)
 
 PRODUCT_NAME := omni_j3xnlte
 PRODUCT_DEVICE := j3xnlte
